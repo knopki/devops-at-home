a342527902b1847fd6ca4891a15e0f17229f992a
Sergey Korolev <korolev.srg@gmail.com>
2018-10-18 21:03:03 +0300

    feat(zsh): create zsh configuration

---
+++ ansible/roles/dotfiles-shell/files/zsh/aliases.zsh
@@ -0,0 +1,11 @@
+alias bc='bc -l -q ${XDG_CONFIG_HOME}/bc'
+alias grep='grep  --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}'
+alias ls='ls --color=auto'
+alias man='nocorrect man'
+alias mv='nocorrect mv'
+alias rsync-copy="rsync -avz --progress -h"
+alias rsync-move="rsync -avz --progress -h --remove-source-files"
+alias rsync-synchronize="rsync -avzu --delete --progress -h"
+alias rsync-update="rsync -avzu --progress -h"
+alias sudo='nocorrect sudo'
+alias fzf='fzf-tmux -m'
---
+++ ansible/roles/dotfiles-shell/files/zsh/fzf.zsh
@@ -0,0 +1,94 @@
+if [ -z $TMUX ]; then
+  FZF_TMUX=1
+fi
+
+alias fzf='fzf-tmux -m'
+
+#FZF_DEFAULT_COMMAND=''
+FZF_DEFAULT_OPTS="--color=dark"
+FZF_COMPLETION_TRIGGER='**'
+#FZF_COMPLETION_OPTS='+c -x'
+#FZF_CTRL_T_COMMAND
+
+
+
+# ALT-I - Paste the selected entry from locate output into the command line
+fzf-locate-widget() {
+  local selected
+  if selected=$(locate / | fzf -q "$LBUFFER"); then
+    LBUFFER=$selected
+  fi
+  zle redisplay
+}
+zle     -N    fzf-locate-widget
+bindkey '\ei' fzf-locate-widget
+
+# GIT heart FZF
+is_in_git_repo() {
+  git rev-parse HEAD > /dev/null 2>&1
+}
+
+gf() {
+  is_in_git_repo || return
+  git -c color.status=always status --short |
+  fzf --border -m --ansi --nth 2..,.. \
+    --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' |
+  cut -c4- | sed 's/.* -> //'
+}
+
+gb() {
+  is_in_git_repo || return
+  git branch -a --color=always | grep -v '/HEAD\s' | sort |
+  fzf --border --ansi --multi --tac --preview-window right:70% \
+    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -'$LINES |
+  sed 's/^..//' | cut -d' ' -f1 |
+  sed 's#^remotes/##'
+}
+
+gt() {
+  is_in_git_repo || return
+  git tag --sort -version:refname |
+  fzf --border --multi --preview-window right:70% \
+    --preview 'git show --color=always {} | head -'$LINES
+}
+
+gh() {
+  is_in_git_repo || return
+  git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
+  fzf --border --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
+    --header 'Press CTRL-S to toggle sort' \
+    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES |
+  grep -o "[a-f0-9]\{7,\}"
+}
+
+gr() {
+  is_in_git_repo || return
+  git remote -v | awk '{print $1 "\t" $2}' | uniq |
+  fzf --border --tac \
+    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1} | head -200' |
+  cut -d$'\t' -f1
+}
+
+# pass completion suggested by @d4ndo (#362)
+_fzf_complete_pass() {
+  _fzf_complete '+m' "$@" < <(
+    pwdir=${PASSWORD_STORE_DIR-~/.password-store/}
+    stringsize="${#pwdir}"
+    find "$pwdir" -name "*.gpg" -print |
+        cut -c "$((stringsize + 1))"-  |
+        sed -e 's/\(.*\)\.gpg/\1/'
+  )
+}
+
+# Autocompetion template
+#_fzf_complete_foo() {
+#  _fzf_complete "--multi --reverse --header-lines=3" "$@" < <(
+#    ls -al
+#  )
+#}
+#
+#_fzf_complete_foo_post() {
+#  awk '{print $NF}'
+#}
+#
+#[ -n "$BASH" ] && complete -F _fzf_complete_foo -o default -o bashdefault foo
