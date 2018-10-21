f25a862357d68e5f1a927e3bcd2905e68efbbf0c
Sergey Korolev <korolev.srg@gmail.com>
2018-10-21 07:02:54 +0300

    feat(shell): add zsh alias

--- ansible/roles/dotfiles-shell/files/zsh/aliases.zsh
+++ ansible/roles/dotfiles-shell/files/zsh/aliases.zsh
@@ -1,12 +1,12 @@
-alias bc='bc -l -q ${XDG_CONFIG_HOME}/bc'
+alias _fzf_complete_gopass='_fzf_complete_pass'
+alias fzf='fzf-tmux -m'
 alias grep='grep  --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}'
 alias ls='ls --color=auto'
 alias man='nocorrect man'
 alias mv='nocorrect mv'
+alias o=xdg-open
 alias rsync-copy="rsync -avz --progress -h"
 alias rsync-move="rsync -avz --progress -h --remove-source-files"
 alias rsync-synchronize="rsync -avzu --delete --progress -h"
 alias rsync-update="rsync -avzu --progress -h"
 alias sudo='nocorrect sudo'
-alias fzf='fzf-tmux -m'
-alias _fzf_complete_gopass='_fzf_complete_pass'
