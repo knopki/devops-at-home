0a670bc38d80e9120b9d30035a751160002a98b6
Sergey Korolev <korolev.srg@gmail.com>
2018-11-17 17:55:24 +0300

    feat(zsh): update aliases

--- ansible/roles/dotfiles-shell/files/zsh/aliases.zsh
+++ ansible/roles/dotfiles-shell/files/zsh/aliases.zsh
@@ -1,5 +1,6 @@
 alias _fzf_complete_gopass='_fzf_complete_pass'
 alias fzf='fzf-tmux -m'
+alias gmpv='flatpak run --filesystem=xdg-download io.github.GnomeMpv --enqueue'
 alias grep='grep  --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}'
 alias ls='ls --color=auto'
 alias man='nocorrect man'
@@ -11,3 +12,9 @@ alias rsync-synchronize="rsync -avzu --delete --progress -h"
 alias rsync-update="rsync -avzu --progress -h"
 alias sudo='nocorrect sudo'
 alias svim='sudo -E vim'
+
+# git
+alias gco="git checkout"
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
+alias gst="git status"
+alias gundo="git reset --soft HEAD^"
