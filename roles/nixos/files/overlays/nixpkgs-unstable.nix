32095f2abb6adc4954b207abcfb62ec7bf5631ba
Sergey Korolev <korolev.srg@gmail.com>
2019-02-22 01:20:55 +0300

    chore(nixos): update deps

--- roles/nixos/files/overlays/nixpkgs-unstable.nix
+++ roles/nixos/files/overlays/nixpkgs-unstable.nix
@@ -2,8 +2,8 @@ self: super: {
   unstable = import ((import ../lib/fetchGHTarball.nix) {
     owner = "NixOS";
     repo = "nixpkgs-channels";
-    rev = "2d6f84c1090ae39c58dcec8f35a3ca62a43ad38c"; # 2019-02-07
-    sha256 = "0l8b51lwxlqc3h6gy59mbz8bsvgc0q6b3gf7p3ib1icvpmwqm773";
+    rev = "fa82ebccf66eef185d063d49a9e294a7a1e15d36"; # 2019-02-22
+    sha256 = "1clkiyx8qljwq0b09l9l6qls8m0ffz059qfkraxzhmjgzj7mpkdw";
   }) {
     config.allowUnfree = true;
   };
