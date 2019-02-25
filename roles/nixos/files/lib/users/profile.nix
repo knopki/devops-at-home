2e559a9e0ae8692bf63a8cb7d1d6c1cca49c4bb7
Sergey Korolev <korolev.srg@gmail.com>
2019-02-25 04:54:01 +0300

    fix(nixos): remove problematic lines from .profile

--- roles/nixos/files/lib/users/profile.nix
+++ roles/nixos/files/lib/users/profile.nix
@@ -4,17 +4,6 @@
     ".profile".text = ''
       # fix systemctl from sudo
       export DBUS_SESSION_BUS_ADDRESS=unix:path=$XDG_RUNTIME_DIR/bus
-
-      # create tmux temporary directory if not exist
-      [ -d $TMUX_TMPDIR ] || mkdir -p $TMUX_TMPDIR
-
-      # If running bash
-      if [ -n "$BASH_VERSION" ]; then
-        # include .bashrc if it exists
-        if [ -f "$HOME/.bashrc" ]; then
-          . "$HOME/.bashrc"
-        fi
-      fi
     '';
   };
 }
