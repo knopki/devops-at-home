e24f6f20a8b98ad137d9f47d68c2e4bad280c1e2
Sergey Korolev <korolev.srg@gmail.com>
2019-08-10 03:21:10 +0300

    feat(gnome3): setup gtk bookmarks

--- roles/nixos/files/modules/users/home/gnome.nix
+++ roles/nixos/files/modules/users/home/gnome.nix
@@ -5,6 +5,19 @@ with lib; {
   config = mkIf config.local.gnome.enable {
     home.packages = with pkgs; [ gnome3.rhythmbox ];
 
+    home.file = {
+      "${config.xdg.configHome}/gtk-3.0/bookmarks".text = ''
+        file://${config.home.homeDirectory}/dev Development
+        file://${config.home.homeDirectory}/docs Documents
+        file://${config.home.homeDirectory}/downloads Downloads
+        file://${config.home.homeDirectory}/music Music
+        file://${config.home.homeDirectory}/pics Pictures
+        file://${config.home.homeDirectory}/videos Videos
+        file://${config.home.homeDirectory}/library Library
+        file://${config.home.homeDirectory}/trash Trash
+      '';
+    };
+
     dconf = {
       enable = true;
       settings = {
