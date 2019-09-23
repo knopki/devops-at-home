5dd3a013d2977c5448859f7f61be9f326f12773e
Sergey Korolev <korolev.srg@gmail.com>
2019-09-24 00:26:16 +0300

    feat(morph): add health checks

--- nix/deploy.nix
+++ nix/deploy.nix
@@ -2,6 +2,16 @@ let
   fetchFromGitHub = (import <nixpkgs> {}).fetchFromGitHub;
   versions = builtins.fromJSON (builtins.readFile ./pkgs/versions.json);
   pkgs = import (fetchFromGitHub versions.nixpkgs-stable) {};
+  desktopHealthChecks = [
+    {
+      cmd = [ "systemctl" "is-active" "--quiet" "home-manager-root.service" ];
+      description = "Check Home Manager activation for root user";
+    }
+    {
+      cmd = [ "systemctl" "is-active" "--quiet" "home-manager-sk.service" ];
+      description = "Check Home Manager activation for sk user";
+    }
+  ];
 in
 {
   network = {
@@ -11,9 +21,11 @@ in
 
   "root@z.alien.1984.run" = { config, ... }: {
     imports = [ ./config/alien.1984.run.nix ];
+    deployment = { healthChecks = { cmd = desktopHealthChecks; }; };
   };
 
   "root@z.panzer.1984.run" = { config, ... }: {
     imports = [ ./config/panzer.1984.run.nix ];
+    deployment = { healthChecks = { cmd = desktopHealthChecks; }; };
   };
 }
