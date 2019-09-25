46d7839754b2a1e70241f3f5eb6bac4ca1d1fd93
Sergey Korolev <korolev.srg@gmail.com>
2019-09-25 23:14:09 +0300

    tweak(nixos): add generic type machine

--- nix/modules/hardware/machine.nix
+++ nix/modules/hardware/machine.nix
@@ -5,6 +5,8 @@ with lib;
 {
   options.local.hardware.machine = mkOption {
     description = "The machine name (usually model).";
-    type = types.enum [ "alienware-15r2" "kvm" "thinkpad-T430s" ];
+    type = types.enum [ "generic" "kvm" "alienware-15r2" "thinkpad-T430s" ];
   };
+
+  config.local.hardware.machine = mkDefault "generic";
 }
