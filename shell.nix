31b3eca4415be6178131529086823617163e8e2c
Sergey Korolev <korolev.srg@gmail.com>
2019-11-02 01:18:25 +0300

    tweak(nix-shell): use nur version of morph

--- shell.nix
+++ shell.nix
@@ -1,7 +1,14 @@
 { sources ? import ./nix/sources.nix }:
 let
+  nur-no-pkgs = import sources.nur {
+    repoOverrides = {
+      knopki = import sources.nur-knopki {};
+      # knopki = import ../nixexprs {};
+    };
+  };
   pkgs = import sources.nixpkgs {
     overlays = [
+      nur-no-pkgs.repos.knopki.overlays.morph
       (
         self: super: {
           unstable = import sources.nixpkgs-unstable {
@@ -15,7 +22,6 @@ let
           niv = if (hasPrefix "0.2." super.haskellPackages.niv.version) then super.haskellPackages.niv else if (hasAttrByPath [ "unstable" "haskellPackages" "niv" ] pkgs) then super.unstable.haskellPackages.niv else (import sources.niv {}).niv;
           # until 20.03 try to use unstable or upstream version
           nixpkgs-fmt = if (hasPrefix "0.6." super.nixpkgs-fmt.version) then super.nixfmt-pkgs else if (hasAttrByPath [ "unstable" "nixpkgs-fmt" ] pkgs) then super.unstable.nixpkgs-fmt else (import sources.nixpkgs-fmt {});
-          morph = super.callPackage "${sources.morph}/nix-packaging" {};
         }
       )
     ];
