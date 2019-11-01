f93e7e525bfee0e3048dceea4dfc4df0c00f1474
Sergey Korolev <korolev.srg@gmail.com>
2019-11-02 02:55:42 +0300

    tweak(nix-shell): use nixpkgs-fmt version of niv

--- shell.nix
+++ shell.nix
@@ -10,19 +10,7 @@ let
     overlays = [
       nur-no-pkgs.repos.knopki.overlays.morph
       nur-no-pkgs.repos.knopki.overlays.niv
-      (
-        self: super: {
-          unstable = import sources.nixpkgs-unstable {
-            config.allowUnfree = true;
-          };
-        }
-      )
-      (
-        self: super: with super.lib; {
-          # until 20.03 try to use unstable or upstream version
-          nixpkgs-fmt = if (hasPrefix "0.6." super.nixpkgs-fmt.version) then super.nixfmt-pkgs else if (hasAttrByPath [ "unstable" "nixpkgs-fmt" ] pkgs) then super.unstable.nixpkgs-fmt else (import sources.nixpkgs-fmt {});
-        }
-      )
+      nur-no-pkgs.repos.knopki.overlays.nixpkgs-fmt
     ];
   };
 in
