e5cefed38cbd4856452332139491ecd1b5cca46c
Sergey Korolev <korolev.srg@gmail.com>
2020-06-27 18:22:59 +0300

    chore: dirty enable flakes

--- nix/modules/roles/workstation.nix
+++ nix/modules/roles/workstation.nix
@@ -24,5 +24,7 @@ with lib;
         notificationsCommand = "sudo -u sk DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus /etc/profiles/per-user/sk/bin/notify-send";
       };
     };
+
+    nix.package = pkgs.nixFlakes;
   };
 }
