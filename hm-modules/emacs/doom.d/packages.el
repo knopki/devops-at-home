f85800a9673105b48706cf35234a0d1ef8b97993
Sergey Korolev <korolev.srg@gmail.com>
2020-12-16 02:59:48 +0300

    chore: update deps

--- hm-modules/emacs/doom.d/packages.el
+++ hm-modules/emacs/doom.d/packages.el
@@ -2,15 +2,14 @@
 ;;; Commentary:
 ;;; Code:
 
-(package! org-ql :pin "00800556907408ee11a23de3a4982061d08d3b85") ;; 0.4.6
-(package! org-super-agenda :pin "3264255989021b8563ea42b5d26acbc2a024f14d")
+(package! org-ql :pin "3924b023e0f1e8fa93cfa79bdd71f9f0c0fb4679") ;; 0.5.0
+(package! org-roam-server :pin "fe0364ef63928337f442c1d987d17cfe9619df2d") ;; 1.0.4
 (package! reverse-im :pin "90add9a1f8c4a3c78029d38087ff4d22fe5372d3")
 (package! russian-holidays :pin "b285a30f29d85c48e3ea4eb93972d34a090c167b")
 (package! wakatime-mode :pin "7626678315918bdbb81ede68149f20a7d97a928f")
 
-;; from https://github.com/hlissner/doom-emacs/pull/2888
-(package! org-ref :pin "2a91b6f67dc3116b0b688c1242ce67d90d833326")
-(package! org-roam-bibtex :pin "7c08a8928967a683666dbc1850e41537ab1fa01a")
+(package! org-ref :pin "120509c38929cc25d814e9a42092c44cb34ec34e") ;; 2020-12-12
+(package! org-roam-bibtex :pin "7c08a8928967a683666dbc1850e41537ab1fa01a") ;; 0.4.0
 
 ;;; Local Variables:
 ;;; byte-compile-warnings: (not free-vars unresolved)
