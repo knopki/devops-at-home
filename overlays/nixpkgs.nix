9a96ea8cc0ff64cae47ef7ed02bc2173256fb223
Sergey Korolev <korolev.srg@gmail.com>
2020-12-28 00:50:37 +0300

    fix: tor browser not broken anymore

--- overlays/nixpkgs.nix
+++ overlays/nixpkgs.nix
@@ -1,6 +1,1 @@
-final: prev: {
-  # TODO: https://github.com/NixOS/nixpkgs/issues/83096
-  tor-browser-bundle-bin = prev.tor-browser-bundle-bin.overrideAttrs (o: {
-    meta = o.meta // { broken = false; };
-  });
-}
+final: prev: { }
