d3e7b9ef777645ff0e468fdd03bf6f6d96a94f86
Sergey Korolev <korolev.srg@gmail.com>
2020-12-29 16:59:02 +0300

    fix: tmpfiles

--- users/sk.nix
+++ users/sk.nix
@@ -166,7 +166,6 @@ in
 
     systemd.user.tmpfiles.rules = [
       "e ${selfHM.xdg.userDirs.download} - - - 30d"
-      "e ${selfHM.xdg.userDirs.download}/*.torrent - - - 1d"
       "e ${selfHM.xdg.userDirs.pictures}/screenshots - - - 30d"
     ];
   };
