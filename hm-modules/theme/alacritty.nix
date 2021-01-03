9c691b3e242a0dcc9c0a112f1b2996ec2705d397
Sergey Korolev <korolev.srg@gmail.com>
2021-01-03 09:53:20 +0300

    fix(alacritty): mkDefault for theme colors

--- hm-modules/theme/alacritty.nix
+++ hm-modules/theme/alacritty.nix
@@ -10,44 +10,44 @@ in
     programs.alacritty.settings = {
       colors = with cfg.base16.colors; {
         primary = {
-          background = "0x${base00.hex.rgb}";
-          foreground = "0x${base05.hex.rgb}";
+          background = mkDefault "0x${base00.hex.rgb}";
+          foreground = mkDefault "0x${base05.hex.rgb}";
         };
 
         cursor = {
-          text = "0x${base00.hex.rgb}";
-          cursor = "0x${base05.hex.rgb}";
+          text = mkDefault "0x${base00.hex.rgb}";
+          cursor = mkDefault "0x${base05.hex.rgb}";
         };
 
         normal = {
-          black = "0x${base00.hex.rgb}";
-          red = "0x${base08.hex.rgb}";
-          green = "0x${base0B.hex.rgb}";
-          yellow = "0x${base0A.hex.rgb}";
-          blue = "0x${base0D.hex.rgb}";
-          magenta = "0x${base0E.hex.rgb}";
-          cyan = "0x${base0C.hex.rgb}";
-          white = "0x${base05.hex.rgb}";
+          black = mkDefault "0x${base00.hex.rgb}";
+          red = mkDefault "0x${base08.hex.rgb}";
+          green = mkDefault "0x${base0B.hex.rgb}";
+          yellow = mkDefault "0x${base0A.hex.rgb}";
+          blue = mkDefault "0x${base0D.hex.rgb}";
+          magenta = mkDefault "0x${base0E.hex.rgb}";
+          cyan = mkDefault "0x${base0C.hex.rgb}";
+          white = mkDefault "0x${base05.hex.rgb}";
         };
 
         bright = {
-          black = "0x${base03.hex.rgb}";
-          red = "0x${base08.hex.rgb}";
-          green = "0x${base0B.hex.rgb}";
-          yellow = "0x${base0A.hex.rgb}";
-          blue = "0x${base0D.hex.rgb}";
-          magenta = "0x${base0E.hex.rgb}";
-          cyan = "0x${base0C.hex.rgb}";
-          white = "0x${base07.hex.rgb}";
+          black = mkDefault "0x${base03.hex.rgb}";
+          red = mkDefault "0x${base08.hex.rgb}";
+          green = mkDefault "0x${base0B.hex.rgb}";
+          yellow = mkDefault "0x${base0A.hex.rgb}";
+          blue = mkDefault "0x${base0D.hex.rgb}";
+          magenta = mkDefault "0x${base0E.hex.rgb}";
+          cyan = mkDefault "0x${base0C.hex.rgb}";
+          white = mkDefault "0x${base07.hex.rgb}";
         };
 
         indexed_colors = [
-          { index = 16; color = "0x${base03.hex.rgb}"; }
-          { index = 17; color = "0x${base03.hex.rgb}"; }
-          { index = 18; color = "0x${base03.hex.rgb}"; }
-          { index = 19; color = "0x${base03.hex.rgb}"; }
-          { index = 20; color = "0x${base03.hex.rgb}"; }
-          { index = 21; color = "0x${base03.hex.rgb}"; }
+          { index = 16; color = mkDefault "0x${base03.hex.rgb}"; }
+          { index = 17; color = mkDefault "0x${base03.hex.rgb}"; }
+          { index = 18; color = mkDefault "0x${base03.hex.rgb}"; }
+          { index = 19; color = mkDefault "0x${base03.hex.rgb}"; }
+          { index = 20; color = mkDefault "0x${base03.hex.rgb}"; }
+          { index = 21; color = mkDefault "0x${base03.hex.rgb}"; }
         ];
       };
     };
