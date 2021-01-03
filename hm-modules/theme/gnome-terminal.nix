81e101b99ea4268aece5c6d5e80ca1249a111ab7
Sergey Korolev <korolev.srg@gmail.com>
2021-01-03 11:03:27 +0300

    tweak(gnome-terminal): theming

--- hm-modules/theme/gnome-terminal.nix
+++ hm-modules/theme/gnome-terminal.nix
@@ -7,35 +7,34 @@ in
   options.theme.components.gnome-terminal.enable = mkEnableOption "Apply theme to Gnome Terminal" // { default = cfg.enable; };
 
   config = mkIf (cfg.enable && cfg.components.gnome-terminal.enable) {
-    # @author Robert Helgesson
     programs.gnome-terminal = {
       themeVariant = cfg.base16.kind;
-      profile."b1dcc9dd-5262-4d8d-a863-c897e6d979b9" = {
+      profile."ea538469-ab30-4a5c-8874-5eca6ab1f081" = with cfg.base16.colors; {
         visibleName = mkDefault "Home Manager - Base16";
         colors = {
-          backgroundColor = mkDefault "#${colors.base00.hex.rgb}";
-          foregroundColor = mkDefault "#${colors.base05.hex.rgb}";
+          backgroundColor = mkDefault "#${base00.hex.rgb}";
+          foregroundColor = mkDefault "#${base05.hex.rgb}";
           cursor = {
-            background = mkDefault "#${colors.base05.hex.rgb}";
-            foreground = mkDefault "#${colors.base00.hex.rgb}";
+            background = mkDefault "#${base05.hex.rgb}";
+            foreground = mkDefault "#${base00.hex.rgb}";
           };
           palette = mkDefault [
-            "#${colors.base00.hex.rgb}"
-            "#${colors.base08.hex.rgb}"
-            "#${colors.base0B.hex.rgb}"
-            "#${colors.base0A.hex.rgb}"
-            "#${colors.base0D.hex.rgb}"
-            "#${colors.base0E.hex.rgb}"
-            "#${colors.base0C.hex.rgb}"
-            "#${colors.base05.hex.rgb}"
-            "#${colors.base03.hex.rgb}"
-            "#${colors.base09.hex.rgb}"
-            "#${colors.base01.hex.rgb}"
-            "#${colors.base02.hex.rgb}"
-            "#${colors.base04.hex.rgb}"
-            "#${colors.base06.hex.rgb}"
-            "#${colors.base0F.hex.rgb}"
-            "#${colors.base07.hex.rgb}"
+            "#${base00.hex.rgb}"
+            "#${base08.hex.rgb}"
+            "#${base0B.hex.rgb}"
+            "#${base0A.hex.rgb}"
+            "#${base0D.hex.rgb}"
+            "#${base0E.hex.rgb}"
+            "#${base0C.hex.rgb}"
+            "#${base05.hex.rgb}"
+            "#${base03.hex.rgb}"
+            "#${base09.hex.rgb}"
+            "#${base01.hex.rgb}"
+            "#${base02.hex.rgb}"
+            "#${base04.hex.rgb}"
+            "#${base06.hex.rgb}"
+            "#${base0F.hex.rgb}"
+            "#${base07.hex.rgb}"
           ];
         };
       };
