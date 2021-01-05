5b3a0037d26cdf5fec6d7b7112ce82b44ae8f346
Sergey Korolev <korolev.srg@gmail.com>
2021-01-05 04:10:46 +0300

    feat(mako): theming support

--- hm-modules/swaywm/mako.nix
+++ hm-modules/swaywm/mako.nix
@@ -6,28 +6,13 @@ with lib;
       enable = true;
       sort = "+time";
       maxVisible = 10;
-      groupBy = "app-name";
       layer = "overlay";
       font = "pango:Noto Sans 10";
       borderRadius = 5;
-      ## Base16 OneDark / Author: Lalit Magant (http://github.com/tilal6991)
-      backgroundColor = "#282c34";
-      textColor = "#abb2bf";
-      borderColor = "#61afef";
     };
 
     xdg.configFile."mako/config" = {
       text = mkAfter ''
-        [urgency=low]
-        background-color=#282c34
-        text-color=#e5c07b
-        border-color=#61afef
-
-        [urgency=high]
-        background-color=#282c34
-        text-color=#e06c75
-        border-color=#61afef
-
         [app-name="Spotify"]
         default-timeout=10000
       '';
@@ -46,7 +31,7 @@ with lib;
         };
         Service = {
           Type = "simple";
-          Restart="always";
+          Restart = "always";
           ExecStart = "${pkgs.mako}/bin/mako";
         };
         Install = { WantedBy = [ "sway-session.target" ]; };
---
+++ hm-modules/theme/mako.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, nixosConfig, ... }:
+with lib;
+let
+  cfg = config.theme;
+in
+{
+  options.theme.components.mako.enable = mkEnableOption "Apply theme to Mako" // { default = cfg.enable; };
+
+  config = mkIf (cfg.enable && cfg.components.mako.enable) (with cfg.base16.colors; {
+    programs.mako = {
+      backgroundColor = mkDefault "#${base00.hex.rgb}";
+      textColor = mkDefault "#${base05.hex.rgb}";
+      borderColor = mkDefault "#${base0D.hex.rgb}";
+    };
+
+
+    xdg.configFile."mako/config".text = mkAfter ''
+      [urgency=low]
+      background-color=#${base00.hex.rgb}
+      text-color=#${base04.hex.rgb}
+      border-color=#${base0D.hex.rgb}
+
+      [urgency=high]
+      background-color=#${base01.hex.rgb}
+      text-color=#${base06.hex.rgb}
+      border-color=#${base08.hex.rgb}
+    '';
+  });
+}
