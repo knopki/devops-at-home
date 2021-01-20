a62eefac5764379b3a9006cbaefb514e24dcd5cc
Sergey Korolev <korolev.srg@gmail.com>
2021-01-20 14:15:43 +0300

    chore: update deps

--- hm-modules/emacs/doom.d/packages.el
+++ hm-modules/emacs/doom.d/packages.el
@@ -4,12 +4,17 @@
 
 (package! org-ql :pin "3924b023e0f1e8fa93cfa79bdd71f9f0c0fb4679") ;; 0.5.0
 (package! org-roam-server :pin "fe0364ef63928337f442c1d987d17cfe9619df2d") ;; 1.0.4
+(package! org-super-agenda :pin "857783ecd3dbe35c72b4eca046e0a5dc64041fdf") ;; 1.2
 (package! reverse-im :pin "90add9a1f8c4a3c78029d38087ff4d22fe5372d3")
 (package! russian-holidays :pin "b285a30f29d85c48e3ea4eb93972d34a090c167b")
 (package! wakatime-mode :pin "7626678315918bdbb81ede68149f20a7d97a928f")
 
 (package! org-ref :pin "120509c38929cc25d814e9a42092c44cb34ec34e") ;; 2020-12-12
 (package! org-roam-bibtex :pin "7c08a8928967a683666dbc1850e41537ab1fa01a") ;; 0.4.0
+
+;; temporary build fix
+(when (featurep! +gnuplot)
+  (package! gnuplot :pin "f0001c30010b2899e36d7d89046322467e923088"))
 
 ;;; Local Variables:
 ;;; byte-compile-warnings: (not free-vars unresolved)
