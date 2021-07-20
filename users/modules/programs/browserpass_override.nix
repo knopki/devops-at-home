9c7dcc28e0afb249207e715e10447bb2236f3124
Sergey Korolev <korolev.srg@gmail.com>
2021-07-20 15:25:21 +0300

    refactor browserpass module

--- users/modules/programs/browserpass_override.nix
+++ users/modules/programs/browserpass_override.nix
@@ -2,7 +2,14 @@
 
 with lib;
 
-let browsers = [ "chrome" "chromium" "firefox" "vivaldi" "brave" ];
+let
+  browsers = [ "chrome" "chromium" "firefox" "vivaldi" "brave" ];
+  mkChromiumFiles = dir: {
+    "${dir}/com.github.browserpass.native.json".source =
+      "${pkgs.browserpass}/lib/browserpass/hosts/chromium/com.github.browserpass.native.json";
+    "${dir}/../policies/managed/com.github.browserpass.native.json".source =
+      "${pkgs.browserpass}/lib/browserpass/policies/chromium/com.github.browserpass.native.json";
+  };
 in {
   disabledModules = [ "programs/browserpass.nix" ];
   options = {
@@ -27,28 +34,14 @@ in {
             "Library/Application Support/Google/Chrome/NativeMessagingHosts"
           else
             ".config/google-chrome/NativeMessagingHosts";
-        in [{
-          "${dir}/com.github.browserpass.native.json".source =
-            "${pkgs.browserpass}/lib/browserpass/hosts/chromium/com.github.browserpass.native.json";
-          "${dir}/../policies/managed/com.github.browserpass.native.json".source =
-            "${pkgs.browserpass}/lib/browserpass/policies/chromium/com.github.browserpass.native.json";
-        }]
+        in [(mkChromiumFiles dir)]
       else if x == "chromium" then
         let
           dir = if isDarwin then
             "Library/Application Support/Chromium/NativeMessagingHosts"
           else
             ".config/chromium/NativeMessagingHosts";
-        in [
-          {
-            "${dir}/com.github.browserpass.native.json".source =
-              "${pkgs.browserpass}/lib/browserpass/hosts/chromium/com.github.browserpass.native.json";
-          }
-          {
-            "${dir}/../policies/managed/com.github.browserpass.native.json".source =
-              "${pkgs.browserpass}/lib/browserpass/policies/chromium/com.github.browserpass.native.json";
-          }
-        ]
+        in [(mkChromiumFiles dir)]
       else if x == "firefox" then
         let
           dir = if isDarwin then
@@ -65,24 +58,14 @@ in {
             "Library/Application Support/Vivaldi/NativeMessagingHosts"
           else
             ".config/vivaldi/NativeMessagingHosts";
-        in [{
-          "${dir}/com.github.browserpass.native.json".source =
-            "${pkgs.browserpass}/lib/browserpass/hosts/chromium/com.github.browserpass.native.json";
-          "${dir}/../policies/managed/com.github.browserpass.native.json".source =
-            "${pkgs.browserpass}/lib/browserpass/policies/chromium/com.github.browserpass.native.json";
-        }]
+        in [(mkChromiumFiles dir)]
       else if x == "brave" then
         let
           dir = if isDarwin then
             "Library/Application Support/BraveSoftware/Brave-Browser/NativeMessagingHosts"
           else
             ".config/BraveSoftware/Brave-Browser/NativeMessagingHosts";
-        in [{
-          "${dir}/com.github.browserpass.native.json".source =
-            "${pkgs.browserpass}/lib/browserpass/hosts/chromium/com.github.browserpass.native.json";
-          "${dir}/../policies/managed/com.github.browserpass.native.json".source =
-            "${pkgs.browserpass}/lib/browserpass/policies/chromium/com.github.browserpass.native.json";
-        }]
+        in [(mkChromiumFiles dir)]
       else
         throw "unknown browser ${x}") config.programs.browserpass.browsers);
   };
