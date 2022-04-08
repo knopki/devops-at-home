45a6cd8c70c94c485ec67a10bc10927ffeafa8fd
Sergey Korolev <korolev.srg@gmail.com>
2022-04-08 18:31:36 +0300

    feat: add containerisation tools

--- profiles/ws-virtualization.nix
+++ profiles/ws-virtualization.nix
@@ -2,31 +2,42 @@
 let inherit (lib) mkDefault; in
 {
   environment = {
+    etc = {
+      "distrobox/distrobox.conf".text = ''
+        container_manager="podman"
+      '';
+    };
+
     sessionVariables = {
       VAGRANT_DEFAULT_PROVIDER = "libvirt";
     };
 
-    systemPackages = with pkgs; [ virt-manager ];
+    systemPackages = with pkgs; [
+      dive
+      distrobox
+      virt-manager
+      podman
+      podman-compose
+    ];
   };
 
   virtualisation = {
     containers.enable = true;
 
-    docker = {
-      autoPrune.dates = mkDefault "weekly";
-      autoPrune.enable = mkDefault true;
-      enable = mkDefault true;
-      enableOnBoot = mkDefault false;
-      liveRestore = mkDefault true;
-    };
     libvirtd = {
       enable = true;
       qemu.runAsRoot = false;
       allowedBridges = [ "virbr0" "virbr1" ];
     };
 
-    oci-containers.backend = "podman";
+    oci-containers.backend = mkDefault "podman";
 
-    podman.enable = true;
+    podman = {
+      enable = true;
+      dockerCompat = mkDefault true;
+      dockerSocket.enable = true;
+    };
+
+
   };
 }
--- users/profiles/programs/cloud-tools.nix
+++ users/profiles/programs/cloud-tools.nix
@@ -1,9 +1,6 @@
 { config, pkgs, ... }:
 {
   home.packages = with pkgs; [
-    docker
-    docker-compose
-    dive
     fishPlugins.fish-kubectl-completions
     google-cloud-sdk
     kubectl
