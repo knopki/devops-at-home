9415582c88965957477f5fbcfc48b4bbd4921b67
Sergey Korolev <korolev.srg@gmail.com>
2022-10-30 03:08:39 +0500

    chore: remove steam

--- nixos/default.nix
+++ nixos/default.nix
@@ -67,7 +67,6 @@ in
         desktop.essentials
         desktop.kde
         fonts
-        games.steam
         misc.earlyoom
         security.disable-mitigations
       ] ++ (with programs; [ downloaders image-viewers video-player music web ]);
