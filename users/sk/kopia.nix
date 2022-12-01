cc673eb6bbe0098b46ee494e6da4cd4f332ed240
Sergei Korolev <knopki@duck.com>
2022-12-01 23:48:27 +0300

    tweak(kopia): split to multiple timers

--- users/sk/kopia.nix
+++ users/sk/kopia.nix
@@ -21,52 +21,78 @@ let inherit (lib) mkIf hm elem; in
       KOPIA_CONTENT_LOG_DIR_MAX_AGE = "168h";
     };
     jobs = {
-      alien-10m = mkIf (nixosConfig.networking.hostName == "alien") {
-        timer = {
-          OnCalendar = "*:0/10";
-        };
+      alien-logseq = mkIf (nixosConfig.networking.hostName == "alien") {
+        timer = { OnCalendar = "*:0/10"; RandomizedDelaySec = "5m"; };
         snapshots = [
           "${config.home.homeDirectory}/.logseq"
-          "${config.home.homeDirectory}/dev"
           "${config.home.homeDirectory}/org"
           "${config.xdg.configHome}/Logseq/Preferences"
           "${config.xdg.configHome}/Logseq/configs.edn"
-          config.xdg.userDirs.documents
+          "${config.xdg.userDirs.documents}/personal"
         ];
       };
-      alien-daily = mkIf (nixosConfig.networking.hostName == "alien") {
-        timer = {
-          OnCalendar = "daily";
-          RandomizedDelaySec = "3h";
-        };
+      alien-devs = mkIf (nixosConfig.networking.hostName == "alien") {
+        timer = { OnCalendar = "*:0/10"; RandomizedDelaySec = "5m"; };
         snapshots = [
-          "${config.home.homeDirectory}/.gnupg"
-          "${config.home.homeDirectory}/.kube/config"
-          "${config.home.homeDirectory}/library"
-          "${config.home.homeDirectory}/trash"
-          "${config.home.homeDirectory}/videos"
-          "${config.xdg.configHome}/Bitwarden/data.json"
-          "${config.xdg.configHome}/Bitwarden CLI/data.json"
+          "${config.home.homeDirectory}/dev"
+        ];
+      };
+      alien-browsers = mkIf (nixosConfig.networking.hostName == "alien") {
+        timer = { OnCalendar = "daily"; RandomizedDelaySec = "12h"; };
+        snapshots = [
           "${config.xdg.configHome}/BraveSoftware/Brave-Browser/Default"
           "${config.xdg.configHome}/BraveSoftware/Brave-Browser/Profile 1"
           "${config.xdg.configHome}/BraveSoftware/Brave-Browser/Profile 2"
           "${config.xdg.configHome}/BraveSoftware/Brave-Browser/Profile 3"
           "${config.xdg.configHome}/BraveSoftware/Brave-Browser/Profile 4"
-          "${config.xdg.configHome}/MusicBrainz"
-          "${config.xdg.configHome}/cachix"
-          "${config.xdg.configHome}/darktable"
-          "${config.xdg.configHome}/dconf/user"
+        ];
+      };
+      alien-secrets = mkIf (nixosConfig.networking.hostName == "alien") {
+        timer = { OnCalendar = "hourly"; RandomizedDelaySec = "30m"; };
+        snapshots = [
+          "${config.home.homeDirectory}/.gnupg"
+          "${config.home.homeDirectory}/.kube/config"
+          "${config.xdg.configHome}/Bitwarden/data.json"
+          "${config.xdg.configHome}/Bitwarden CLI/data.json"
           "${config.xdg.configHome}/gcloud"
           "${config.xdg.configHome}/remmina"
           "${config.xdg.configHome}/teamviewer"
-          "${config.xdg.dataHome}/Anki2"
+          "${config.xdg.configHome}/cachix"
           "${config.xdg.dataHome}/fish/fish_history"
           "${config.xdg.dataHome}/keyrings"
           "${config.xdg.dataHome}/password-store"
           "${config.xdg.dataHome}/remmina"
+        ];
+      };
+      alien-docs = mkIf (nixosConfig.networking.hostName == "alien") {
+        timer = { OnCalendar = "daily"; RandomizedDelaySec = "12h"; };
+        snapshots = [
+          "${config.home.homeDirectory}/library"
+          "${config.home.homeDirectory}/trash"
           config.xdg.userDirs.desktop
+          config.xdg.userDirs.documents
+        ];
+      };
+      alien-media = mkIf (nixosConfig.networking.hostName == "alien") {
+        timer = { OnCalendar = "daily"; RandomizedDelaySec = "12h"; };
+        snapshots = [
+          "${config.home.homeDirectory}/videos"
+          "${config.xdg.configHome}/darktable"
           config.xdg.userDirs.music
           config.xdg.userDirs.pictures
+        ];
+      };
+      alien-anki = mkIf (nixosConfig.networking.hostName == "alien") {
+        timer = { OnCalendar = "weekly"; RandomizedDelaySec = "24h"; };
+        snapshots = [
+          "${config.xdg.dataHome}/Anki2"
+        ];
+      };
+      alien-random-conf = mkIf (nixosConfig.networking.hostName == "alien") {
+        timer = { OnCalendar = "daily"; RandomizedDelaySec = "12h"; };
+        snapshots = [
+          "${config.xdg.configHome}/MusicBrainz"
+          "${config.xdg.configHome}/dconf/user"
         ];
       };
     };
