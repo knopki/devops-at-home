2510ebeb323ee66c623d718b696edc483c9d2d57
Sergei Korolev <knopki@duck.com>
2022-12-22 05:13:45 +0300

    chore: remove useless overrides

--- pkgs/overlays/overrides.nix
+++ pkgs/overlays/overrides.nix
@@ -3,19 +3,4 @@ channels: final: prev: {
   __dontExport = true; # overrides clutter up actual creations
 
   # inherit (channels.latest) something;
-
-  haskellPackages = prev.haskellPackages.override {
-    overrides = hfinal: hprev:
-      let version = prev.lib.replaceChars [ "." ] [ "" ] prev.ghc.version;
-      in
-      {
-        # same for haskell packages, matching ghc versions
-        inherit (channels.latest.haskell.packages."ghc${version}")
-          haskell-language-server;
-      };
-  };
-
-  nodePackages = prev.nodePackages // {
-    pyright = channels.latest.nodePackages.pyright;
-  };
 }
