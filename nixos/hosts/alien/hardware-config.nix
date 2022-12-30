0bd04b372327f0a7db9377582d3acb750df3507f
Sergei Korolev <knopki@duck.com>
2022-12-30 11:19:36 +0300

    feat: enable memtest

--- nixos/hosts/alien/hardware-config.nix
+++ nixos/hosts/alien/hardware-config.nix
@@ -62,7 +62,10 @@ in
 
     loader = {
       efi.canTouchEfiVariables = true;
-      systemd-boot.enable = true;
+      systemd-boot = {
+        enable = true;
+        memtest86.enable = true;
+      };
     };
   };
 
