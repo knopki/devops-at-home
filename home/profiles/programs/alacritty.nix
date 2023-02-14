44a4830a5d5a663d9973163ffa62660a57df80fe
Sergei Korolev <knopki@duck.com>
2023-02-14 20:58:24 +0300

    chore: remove useless fix for alacritty

--- home/profiles/programs/alacritty.nix
+++ home/profiles/programs/alacritty.nix
@@ -6,10 +6,6 @@ in
   programs.alacritty = {
     enable = mkDefault true;
     settings = {
-      env = {
-        # fix scaling on X11
-        WINIT_X11_SCALE_FACTOR = "1";
-      };
       key_bindings = [
         # spawn a new instance of Alacritty in the current working directory
         { key = "Return"; mods = "Control|Shift"; action = "SpawnNewInstance"; }
