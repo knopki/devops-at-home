614245410decc69659fed78166cb0508289a750c
Sergei Korolev <knopki@duck.com>
2023-03-22 14:51:05 +0300

    fix: syncthingtray delayed startup

--- home/profiles/services/syncthing.nix
+++ home/profiles/services/syncthing.nix
@@ -4,4 +4,19 @@
     enable = true;
     tray.enable = true;
   };
+
+  # delay start
+  systemd.user = {
+    services.${config.services.syncthing.tray.package.pname} = {
+      Install.WantedBy = lib.mkForce [ ];
+    };
+
+    timers.${config.services.syncthing.tray.package.pname} = {
+      Timer = {
+        OnActiveSec = "10s";
+        AccuracySec = "1s";
+      };
+      Install = { WantedBy = [ "graphical-session.target" ]; };
+    };
+  };
 }
