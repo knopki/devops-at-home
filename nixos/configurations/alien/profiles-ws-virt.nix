0538a2f73e441adc116c1b9a948f31cdb7fc5ba2
Sergei Korolev <knopki@duck.com>
2025-01-17 16:23:48 +0300

    tweak(alien): disable libvirt

--- nixos/configurations/alien/profiles-ws-virt.nix
+++ nixos/configurations/alien/profiles-ws-virt.nix
@@ -15,14 +15,8 @@ in
       '';
     };
 
-    sessionVariables = {
-      VAGRANT_DEFAULT_PROVIDER = "libvirt";
-    };
-
     systemPackages = with pkgs; [
       dive
-      libguestfs-with-appliance
-      virt-manager
 
       # docker client (docker compose, for example) can be used with podman
       docker
@@ -33,15 +27,6 @@ in
   virtualisation = {
     containers = {
       enable = true;
-    };
-
-    libvirtd = {
-      enable = true;
-      qemu.runAsRoot = false;
-      allowedBridges = [
-        "virbr0"
-        "virbr1"
-      ];
     };
 
     oci-containers.backend = mkDefault "podman";
