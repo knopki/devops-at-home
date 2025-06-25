b6aff73217d910dc80551dbbab97c4dac05b93ea
Red Buttons <knopki@duck.com>
2025-06-25 13:39:13 +0300

    chore: update deps

--- pkgs/ze/zed-editor/package.nix
+++ pkgs/ze/zed-editor/package.nix
@@ -1,11 +1,10 @@
-{ nixpkgs-25-05, ... }:
-nixpkgs-25-05.zed-editor.fhsWithPackages (
+{ nixpkgsUnstable, ... }:
+nixpkgsUnstable.zed-editor.fhsWithPackages (
   ps: with ps; [
     bash-language-server
     docker-compose-language-service
     dockerfile-language-server-nodejs
     eslint
-    openssl
     pyright
     ruff
     tailwindcss-language-server
