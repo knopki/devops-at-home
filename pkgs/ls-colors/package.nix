36d5ab57b03cc4ba291dfdf347d9a5f860140d5e
Red Buttons <knopki@duck.com>
2025-07-01 20:03:47 +0300

    chore: update package versions

--- pkgs/ls-colors/package.nix
+++ pkgs/ls-colors/package.nix
@@ -6,13 +6,13 @@
 }:
 stdenv.mkDerivation {
   pname = "ls-colors";
-  version = "0-unstable-2024-12-20";
+  version = "0-unstable-2025-06-06";
   src = fetchFromGitHub {
     owner = "trapd00r";
     repo = "LS_COLORS";
-    rev = "81e2ebcdc2ed815d17db962055645ccf2125560c";
+    rev = "810ce8cac886ac50e75d84fb438b549a1f9478ee";
     fetchSubmodules = false;
-    sha256 = "sha256-ePs7UlgQqh3ptRXUNlY/BDa/1aH9q3dGa3h0or/e6Kk=";
+    sha256 = "sha256-MMzNknuELhpSkvcPgCL2Pp5A6DZrLajkz8qLphSNbjY=";
   };
 
   installPhase = ''
--- pkgs/mpv-align-images/package.nix
+++ pkgs/mpv-align-images/package.nix
@@ -5,13 +5,13 @@
 }:
 pkgs.mpvScripts.buildLua {
   pname = "mpv-align-images";
-  version = "0-unstable-2024-11-03";
+  version = "0-unstable-2025-04-07";
   src = fetchFromGitHub {
     owner = "guidocella";
     repo = "mpv-image-config";
-    rev = "f8ba0d22bb738bb0c55545121d525613a92b498d";
+    rev = "5c11efbf1cba194fd025f64091411bb9557d3117";
     fetchSubmodules = false;
-    sha256 = "sha256-xGdbyM0igHhmvmM8g1B0pretts/ajrESf0FQysZY3ig=";
+    sha256 = "sha256-UjbYk9KclNoZNn3Q/7fer4YPq+mmuNJ2RMRhrNl5wOU=";
   };
 
   installPhase = ''
