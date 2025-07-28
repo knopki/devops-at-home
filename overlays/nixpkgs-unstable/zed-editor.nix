cd59f9805e25ea643555e3cb3f2b82f036b6b4f3
Red Buttons <knopki@duck.com>
2025-07-28 18:31:14 +0300

    feat(overlays): zed editor overlay if too old

--- overlays/nixpkgs-unstable/zed-editor.nix
+++ overlays/nixpkgs-unstable/zed-editor.nix
@@ -1,5 +1,13 @@
-final: prev: {
-  zed-editor-fhs = prev.nixpkgsUnstable.zed-editor.fhsWithPackages (_ps: [
+final: prev:
+let
+  inherit (prev.lib.strings) compareVersions;
+  minVersion = "0.196.0";
+  tooOld = compareVersions minVersion prev.zed-editor.version >= 0;
+  zedEditorPkg = if tooOld then prev.nixpkgsUnstable.zed-editor else prev.zed-editor;
+in
+{
+  zed-editor = zedEditorPkg;
+  zed-editor-fhs = zedEditorPkg.fhsWithPackages (_ps: [
     # final. prefix - install from the current pkgs
     # ps. prefix - install from the zed-editor's pkgs
     final.bash-language-server
