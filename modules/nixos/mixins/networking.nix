19647ba767d7155be96ca4199a130083f7f0a45a
Red Buttons <knopki@duck.com>
2025-07-29 16:32:13 +0300

    fix: wi-fi stopped on apply

--- modules/nixos/mixins/networking.nix
+++ modules/nixos/mixins/networking.nix
@@ -18,10 +18,11 @@
   # FIXME: Maybe upstream?
   # Do not take down the network for too long when upgrading,
   # This also prevents failures of services that are restarted instead of stopped.
-  # It will use `systemctl restart` rather than stopping it with `systemctl stop`
-  # followed by a delayed `systemctl start`.
+  # It will use `systemctl restart` rather than stopping it
+  # with `systemctl stop` followed by a delayed `systemctl start`.
   systemd.services.systemd-networkd.stopIfChanged = false;
   systemd.services.NetworkManager.stopIfChanged = false;
+  systemd.services.wpa_supplicant.stopIfChanged = false;
   # Services that are only restarted might be not able to resolve when resolved is stopped before
   systemd.services.systemd-resolved.stopIfChanged = false;
 }
