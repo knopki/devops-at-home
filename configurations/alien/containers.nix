601c76006fe001b063574ad4f9d1e1c1477fa3e8
Red Buttons <knopki@duck.com>
2025-09-19 14:52:31 +0300

    chore(alien): remove torrserver

--- configurations/alien/containers.nix
+++ configurations/alien/containers.nix
@@ -1,24 +1,6 @@
 { ... }:
-let
-  inherit (builtins) toString;
-
-  torrserverPort = 5665;
-in
 {
   virtualisation.oci-containers.containers = {
-    torrserver = {
-      image = "ghcr.io/yourok/torrserver:MatriX.133t";
-      environment = {
-        TS_DONTKILL = "1";
-        TS_HTTPAUTH = "0";
-      };
-      volumes = [
-        "/var/lib/torrserver/torrents:/opt/ts/torrents"
-        "/var/lib/torrserver/config:/opt/ts/config"
-      ];
-      ports = [ "${toString torrserverPort}:8090" ];
-    };
-
     # how to configure:
     #   podman run --rm -it \
     #     -v /var/lib/isponsorblocktv:/app/data \
@@ -27,9 +9,5 @@ in
       image = "ghcr.io/dmunozv04/isponsorblocktv:v2.2.1";
       volumes = [ "/var/lib/isponsorblocktv:/app/data" ];
     };
-  };
-
-  networking.firewall = {
-    allowedTCPPorts = [ torrserverPort ];
   };
 }
