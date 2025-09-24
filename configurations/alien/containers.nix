c2ad1375fc204cf2c979bc056aa3e881d254847a
Red Buttons <knopki@duck.com>
2025-09-24 17:57:45 +0300

    feat(alien): bump isponsorblocktv container version

--- configurations/alien/containers.nix
+++ configurations/alien/containers.nix
@@ -4,9 +4,9 @@
     # how to configure:
     #   podman run --rm -it \
     #     -v /var/lib/isponsorblocktv:/app/data \
-    #     ghcr.io/dmunozv04/isponsorblocktv:v2.2.1 --setup
+    #     ghcr.io/dmunozv04/isponsorblocktv:v2.6.0 --setup
     isponsorblocktv = {
-      image = "ghcr.io/dmunozv04/isponsorblocktv:v2.2.1";
+      image = "ghcr.io/dmunozv04/isponsorblocktv:v2.6.0";
       volumes = [ "/var/lib/isponsorblocktv:/app/data" ];
     };
   };
