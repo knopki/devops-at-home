ae7f373372e606710403a21555fccb295ba86fb8
Red Buttons <knopki@duck.com>
2025-10-20 21:19:24 +0300

    feat: add devenv cachix key

--- modules/nixos/mixins/trusted-nix-caches.nix
+++ modules/nixos/mixins/trusted-nix-caches.nix
@@ -10,6 +10,7 @@
   ++ (self.nixConfig.extra-substituters or [ ]);
   nix.settings.trusted-public-keys = [
     "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
     "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
