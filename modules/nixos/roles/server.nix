51c2625f6f872f9f15e8cae3f7f9e3bfa3cece52
Red Buttons <knopki@duck.com>
2025-12-03 14:38:31 +0300

    fix: watchdog syntax change after 25.11 upgrade

--- modules/nixos/roles/server.nix
+++ modules/nixos/roles/server.nix
@@ -91,20 +91,20 @@ in
 
       # For more detail, see:
       #   https://0pointer.de/blog/projects/watchdog.html
-      watchdog = {
+      settings.Manager = {
         # systemd will send a signal to the hardware watchdog at half
         # the interval defined here, so every 7.5s.
         # If the hardware watchdog does not get a signal for 15s,
         # it will forcefully reboot the system.
-        runtimeTime = mkDefault "15s";
+        RuntimeWatchdogSec = mkDefault "15s";
         # Forcefully reboot if the final stage of the reboot
         # hangs without progress for more than 30s.
         # For more info, see:
         #   https://utcc.utoronto.ca/~cks/space/blog/linux/SystemdShutdownWatchdog
-        rebootTime = mkDefault "30s";
+        RebootWatchdogSec = mkDefault "30s";
         # Forcefully reboot when a host hangs after kexec.
         # This may be the case when the firmware does not support kexec.
-        kexecTime = mkDefault "1m";
+        KExecWatchdogSec = mkDefault "1m";
       };
 
       sleep.extraConfig = ''
