7793e76e0a804b02d1667a5265a48dfab194fdcf
Red Buttons <knopki@duck.com>
2026-06-01 18:50:24 +0300

    feat(rog): software bundle

--- configurations/rog/inbox.nix
+++ configurations/rog/inbox.nix
@@ -1,5 +1,29 @@
 # Temporary configuration that should go away
 # Remove or move to modules/other files
-_: {
+{ pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    iproute2
+    mtr
+    dig
+    curl
+    whois
+    tcpdump
+    wireshark
+    termshark
+    ngrep
+    nmap
+    netcat-openbsd
+    socat
+    iperf3
+    iftop
+    speedtest-cli
+    rsync
+    jq
+  ];
 
+  programs.throne = {
+    enable = true;
+    tunMode.enable = true;
+  };
 }
